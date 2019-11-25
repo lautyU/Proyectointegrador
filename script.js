3ec46dab42c74dc8b8acacdb5855f15973a0c57e
lautyU <47756293+lautyU@users.noreply.github.com>
2019-11-25 17:01:18 -0300

    Html de la pagina principal

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnToggle=document.querySelector('#Toggle');
+btnToggle.addEventListener('click' ,function(){
+document.getElementById('sidebar').classList.toggle('active')
+
+}
+);sdfgsgsf
+// function showStuff(id, text, btn) {
+//     document.getElementById(id).style.display = 'block';
+//     hide the lorem ipsum text
+//     document.getElementById(text).style.display = 'none';
+//     hide the link
+//     btn.style.display = 'none
